896f13481fd3b2f016daa7bf7f9a86d163e9c02c
jordan95v <jordan.dfrsne@gmail.com>
2023-02-19 17:14:06 +0100

    Removed some weird space.

--- main.asm
+++ main.asm
@@ -61,51 +61,51 @@ section .text
 
 global main
 main:
-    xor     rdi,rdi
-    call    XOpenDisplay	; Création de display
-    mov     qword[display_name],rax	; rax=nom du display
+    xor rdi,rdi
+    call XOpenDisplay	; Création de display
+    mov qword[display_name],rax	; rax=nom du display
 
     ; display_name structure
     ; screen = DefaultScreen(display_name);
-    mov     rax,qword[display_name]
-    mov     eax,dword[rax+0xe0]
-    mov     dword[screen],eax
+    mov rax,qword[display_name]
+    mov eax,dword[rax+0xe0]
+    mov dword[screen],eax
 
-    mov rdi,qword[display_name]
-    mov esi,dword[screen]
+    mov rdi, qword[display_name]
+    mov esi, dword[screen]
     call XRootWindow
-    mov rbx,rax
+    mov rbx, rax
 
-    mov rdi,qword[display_name]
-    mov rsi,rbx
-    mov rdx,10
-    mov rcx,10
-    mov r8,400	; largeur
-    mov r9,400	; hauteur
+    mov rdi, qword[display_name]
+    mov rsi, rbx
+    mov rdx, 10
+    mov rcx, 10
+    mov r8, 400	; largeur
+    mov r9, 400	; hauteur
     push 0xFFFFFF	; background  0xRRGGBB
     push 0x00FF00
     push 1
     call XCreateSimpleWindow
     mov qword[window],rax
 
-    mov rdi,qword[display_name]
-    mov rsi,qword[window]
-    mov rdx,131077 ;131072
+    mov rdi, qword[display_name]
+    mov rsi, qword[window]
+    mov rdx, 131077 ;131072
     call XSelectInput
 
-    mov rdi,qword[display_name]
-    mov rsi,qword[window]
+    mov rdi, qword[display_name]
+    mov rsi, qword[window]
     call XMapWindow
 
-    mov rsi,qword[window]
-    mov rdx,0
-    mov rcx,0
+    mov rsi, qword[window]
+    mov rdx, 0
+    mov rcx, 0
     call XCreateGC
     mov qword[gc],rax
 
 boucle: ; boucle de gestion des évènements
-    mov rdi,qword[display_name]
-    mov rsi,event
+    mov rdi, qword[display_name]
+    mov rsi, event
     call XNextEvent
 
     cmp dword[event], ConfigureNotify	; à l'apparition de la fenêtre
@@ -321,7 +321,7 @@ indirect:
 
 ; Fin du dessin.
 flush:
-    mov rdi,qword[display_name]
+    mov rdi, qword[display_name]
     call XFlush
 
     ; Fait en sorte de dessiner le bon nombre de triangle.
@@ -332,14 +332,14 @@ flush:
 
     ; Sinon on jump à la gestion d'évenements.
     jmp boucle
-    mov rax,34
+    mov rax, 34
     syscall
 
 closeDisplay:
-    mov     rax,qword[display_name]
-    mov     rdi,rax
+    mov     rax, qword[display_name]
+    mov     rdi, rax
     call    XCloseDisplay
-    xor	    rdi,rdi
+    xor	    rdi, rdi
     call    exit
 
 global random_number
